8e0f64d91b6e49d182839e8f492efa3d04671a4d
Farhan Apriliansyah <muhamadfarhanapriliansyah29@gmail.com>
2025-02-28 20:52:05 +0700

    update per2

---
+++ pertemuan2/variable_user.php
@@ -0,0 +1,10 @@
+<?php
+//mendefinisikan variabel
+$nama = "Bima";
+$umur = 40;
+$bb = 40;
+
+echo "nama saya adalah : " .$nama;
+echo "<br/>umur : " .$umur. " tahun";
+echo "<br/>berat badan : " .$bb. " kg";
+?>
