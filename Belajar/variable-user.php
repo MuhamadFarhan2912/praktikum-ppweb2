633e570ddc0c16bedc563985dab9853be4c92d74
Farhan Apriliansyah <muhamadfarhanapriliansyah29@gmail.com>
2025-03-13 19:03:44 +0700

    menambahkan folder belajar

---
+++ Belajar/variable-user.php
@@ -0,0 +1,11 @@
+<?php
+$nama='Farhan';
+$umur=20;
+$alamat='kampung cibogel';
+$jurusan='TI';
+
+echo 'nama saya adalah ' .$nama. '<br/>';
+echo 'umur saya adalah ' .$umur.  '<br/>';
+echo 'alamat saya di ' .$alamat.  '<br/>';
+echo 'jursan saya ' .$jurusan.  '<br/>';
+?>
