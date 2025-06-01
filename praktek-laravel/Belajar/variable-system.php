b5cbf4fa3fbda8d137933638883d252a1ca0ca7d
Farhan Apriliansyah <muhamadfarhanapriliansyah29@gmail.com>
2025-06-01 18:20:41 +0700

    Update isi folder praktek-laravel

--- praktek-laravel/Belajar/variable-system.php
+++
@@ -1,6 +0,0 @@
-<?php
-// variable system
-echo 'dokumen root : ' .$_SERVER['DOCUMENT_ROOT'];
-echo '<br/>Nama File : ' .$_SERVER['PHP_SELF']. '<br/>';
-
-?>
--- praktek-laravel/pertemuan2/variable_system.php
+++
@@ -1,5 +0,0 @@
-<?php
-    //variable sistem
-    echo "dokumen root " .$_SERVER["DOCUMENT_ROOT"];
-    echo "<br/>nama file " .$_SERVER["PHP_SELF"];
-?>
